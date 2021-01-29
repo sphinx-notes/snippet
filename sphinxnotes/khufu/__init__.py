dc920cf258902fd42eb8fd682b32c648007218d8
Shengyu Zhang <i@silverrainz.me>
2021-01-29 11:17:14 +0800

    Add entrypoint to setup.py

--- sphinxnotes/khufu/__init__.py
+++ sphinxnotes/khufu/__init__.py
@@ -14,13 +14,14 @@ from typing import List
 
 from .cli import snippet
 
+__version__ = '1.0a0'
 
 def main(argv:List[str]=sys.argv[1:]) -> int:
     """Command line entrypoint."""
 
     parser = argparse.ArgumentParser(prog='khufu',
                                      description='Command line toolset for Sphinx documentation.')
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0a0')
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
     # Add subparsers from sub modules
     subparsers = parser.add_subparsers()
 
