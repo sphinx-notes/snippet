bfdec07049ce08d0aafaa48748c8517b7ce970e2
Shengyu Zhang <i@silverrainz.me>
2021-08-27 17:20:53 +0800

    Skipped document with empty source

--- sphinxnotes/snippet/picker.py
+++ sphinxnotes/snippet/picker.py
@@ -26,6 +26,10 @@ def pick(doctree:nodes.document) -> List[Tuple[Snippet,nodes.section]]:
     Pick snippets from document, return a list of snippet and the section
     it belongs to.
     """
+    if doctree.source is None or doctree.source == "":
+        logger.debug('Skipped document with empty source')
+        return []
+
     snippets = []
 
     # Pick document
